b7428061ee863dd4307cd525403ff39a6e10c8cc
borga <borgaaydin@gmail.com>
2016-06-03 00:20:49 +0200

    header footer

--- js/slider.js
+++ js/slider.js
@@ -30,6 +30,7 @@ $(document).ready(function(){
         variableWidth: true,
         arrows: false,
         autoplay: true,
-        autoplaySpeed: 2000
+        autoplaySpeed: 2000,
+        mobileFirst: true
     });
 });
