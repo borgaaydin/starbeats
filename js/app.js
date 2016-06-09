26cdc2791b775d8d8ecd07d8b1b2fa7c8c1950d5
borga <borgaaydin@gmail.com>
2016-06-09 15:26:09 +0200

    STARBEATS-22 Create Artist Form Image Upload

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
  */
 'use strict';
 
-var starbeats = angular.module('starbeats', [])
+var starbeats = angular.module('starbeats', ['ngFileUpload', 'ngImgCrop'])
     .run(function ($rootScope) {
         $rootScope.API = "http://localhost/";
     });
--- js/controllers/CreateArtistController.js
+++ js/controllers/CreateArtistController.js
@@ -3,18 +3,32 @@
  */
 
 starbeats.controller('CreateArtistController',
-    function CreateArtistController($scope, $http, $rootScope, CreateArtistService){
-        $scope.createArtist = function(artist) {
-            console.log(artist);
-            CreateArtistService.createArtist(artist).then(function (response) {
-                console.log(response);
-            }, function (response) {
-                console.log(response);
-            });
-        };
+    function CreateArtistController($scope, $http, $rootScope, CreateArtistService, Upload){
+        $scope.reveal = false;
 
-        $scope.uploadFlyer = function() {
-            
+        $scope.createArtist = function(artist, createArtistForm) {
+            console.log(artist);
+            if(createArtistForm.$valid) {
+                CreateArtistService.createArtist(artist).then(function (response) {
+                    console.log(response);
+                }, function (response) {
+                    console.log(response);
+                });
+            }
+        };
+        $scope.upload = function (file) {
+            Upload.upload({
+                url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
+                data: {file: file, 'username': $scope.username}
+            }).then(function (resp) {
+                console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
+            }, function (resp) {
+                console.log('Error status: ' + resp.status);
+            }, function (evt) {
+                var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+                console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+            });
         };
     }
 );
+
